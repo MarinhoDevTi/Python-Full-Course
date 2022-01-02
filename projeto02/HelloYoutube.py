5a5877a8f21b0c393ba6a2e10ede5f2f7e2d2c0b
marinho <diogomarinhomg@gmail.com>
2022-01-01 23:27:22 -0300

    Update HelloYoutube.py

--- projeto02/HelloYoutube.py
+++ projeto02/HelloYoutube.py
@@ -1,8 +1,13 @@
-name = "estado paraná"
+name = "estado parana"
 
 #print( len (name))
 #print(name.find("5"))
 #print (name.capitalize())
 #print (name.upper())
 #print( name.lower())
-print (name.isdigit())
+#print (name.isdigit())
+#print (name.isalpha())
+#print (name.count("a"))
+#print (name.replace("a","o"))
+
+
