cb3ad7d97c315fe45631aa78e6a0329b9da11714
marinho <diogomarinhomg@gmail.com>
2022-02-03 22:59:52 -0300

    Update

---
+++ casting/main.py
@@ -0,0 +1,11 @@
+#type casting = convert the data type of a value to another data type
+
+x = 1 #int
+y = 2.0 #float 
+z = "3" #str
+
+print()
+print(x)  
+print(int(y))
+print(z)
+print ()
---
+++ input/main.py
@@ -0,0 +1,9 @@
+name = input("Qual o seu nome?:")
+age = int(input("How old are you?:"))
+
+age = age + 1
+
+print("Hello "+ name)
+print ("You have " +str (age) + " year")
+
+
