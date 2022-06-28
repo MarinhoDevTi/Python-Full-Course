691e23d5a820e449d2331b7459002b84cbe9658c
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-06-28 12:07:45 -0300

    Update main.py

--- exep_01/main.py
+++ exep_01/main.py
@@ -1,7 +1,7 @@
 #variaveis
-nome = (input("Qual o seu nome ?"))
-idade = (input("Qual sua idade ?"))
-altura = (input("Qual a sua Altura ?"))
-peso = (input("Qual o seu Peso ?"))
+nome = (input("Qual o seu nome ? "))
+idade = (input("Qual sua idade ? "))
+altura = (input("Qual a sua Altura ? "))
+peso = (input("Qual o seu Peso ? "))
 
 print (nome, "|", idade, "|", altura, "|", peso)
